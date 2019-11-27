eab39b7e73bb555fc64bdef11cfc2072e11f52f4
Siderlan Santos <siderlan.santos@gmail.com>
2019-11-27 15:09:57 -0300

    Refactor unit test for ApiClient class

--- src/__tests__/api-client.test.ts
+++ src/__tests__/api-client.test.ts
@@ -1,23 +1,25 @@
 import { ApiClient } from '../api-client'
 
+let apiClient: ApiClient
+
+beforeAll(() => {
+  apiClient = new ApiClient()
+})
+
 describe('ApiClient', () => {
   it('Creates an instance of ApiClient', () => {
-    const apiClient = new ApiClient()
     expect(apiClient).toBeInstanceOf(ApiClient)
   })
 
   it('Has an app key', () => {
-    const apiClient = new ApiClient()
     expect(apiClient).toHaveProperty('appKey')
   })
 
   it('Has an app secret', () => {
-    const apiClient = new ApiClient()
     expect(apiClient).toHaveProperty('appSecret')
   })
 
   it('Has an apiCall method', () => {
-    const apiClient = new ApiClient()
     expect(apiClient).toHaveProperty('apiCall')
   })
 })
