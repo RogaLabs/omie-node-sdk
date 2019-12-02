97deb64db37a812a308cbdf45c4bce292af5115e
Siderlan Santos <siderlan.santos@gmail.com>
2019-12-02 15:07:32 -0300

    Add InformacoesAdicionais interface

---
+++ src/interfaces/informacoes-adicionais.ts
@@ -0,0 +1,20 @@
+/**
+ * Omie's advanced type.
+ *
+ * @export
+ * @interface InformacoesAdicionais
+ */
+export interface InformacoesAdicionais {
+  cCodCateg: string;
+  nCodCC: number;
+  cNumPedido: string;
+  cNumContrato?: string;
+  cContato?: string;
+  cDadosAdicNF?: string;
+  cCodObra?: string;
+  cCodART?: string;
+  nCodProj?: number;
+  cCidPrestServ?: string;
+  dDataRps?: string;
+  cNumRecibo?: string;
+}
