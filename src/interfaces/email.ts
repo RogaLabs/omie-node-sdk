381bf98541f2c8f4df63e2933d99919bc9a5f5d9
Siderlan Santos <siderlan.santos@gmail.com>
2019-12-02 15:06:34 -0300

    Add Email interface

---
+++ src/interfaces/email.ts
@@ -0,0 +1,12 @@
+/**
+ * Omie's advanced type.
+ *
+ * @export
+ * @interface Email
+ */
+export interface Email {
+  cEnvRecibo: string;
+  cEnvLink: string;
+  cEnvBoleto: string;
+  cEnviarPara: string;
+}
