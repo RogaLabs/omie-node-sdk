6c36d749bf34481cb1fde96830f385c244544298
Siderlan Santos <siderlan.santos@gmail.com>
2019-12-02 15:07:03 -0300

    Add Parcelas interface

---
+++ src/interfaces/parcelas.ts
@@ -0,0 +1,13 @@
+/**
+ * Omie's advanced type.
+ *
+ * @export
+ * @interface Parcelas
+ */
+export interface Parcelas {
+  nParcela: number;
+  dDtVenc: string;
+  nValor: number;
+  nPercentual: number;
+  nDias: number;
+}
